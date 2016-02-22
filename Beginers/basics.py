93a45d822f835dd271089f52da1445c29bb79b60
Paul Ross <apaulross@gmail.com>
2016-02-22 18:31:35 +0000

    Adds basics.

---
+++ Beginers/basics.py
@@ -0,0 +1,39 @@
+'''
+Created on 22 Feb 2016
+
+@author: paulross
+'''
+# Given
+x = 10000.0
+y = 3.0
+print x / y
+print 10000 / 3
+# What is happening?
+
+# Given
+print x - 1 / y
+print (x - 1) / y
+# What is happening?
+
+# Given 
+x = 'foo'
+y = 'bar'
+# Create 'foobar'
+# Create 'foo -> bar'
+
+# Given
+# 'hello world'
+# print 'HELLO WORLD'
+# print 'hellX wXrld'
+
+# Given
+# x = 10000.0
+# y = 3.0
+# print "10000 / 3 = 3333"
+
+# Given
+# ['hello', 'world']
+# print 'helloworld'
+# print 'hello, world'
+# print 'hello
+# world'
